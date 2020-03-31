7ac8f1178066ba5e78689cfab3ecd1bb4cacd60c
Angelo Elias Dalzotto <angelodalzotto97@gmail.com>
2020-03-31 14:19:52 -0300

    Proper Python shebang/enhance distro compatibility

--- build_env/scripts/deloream_env.py
+++ build_env/scripts/deloream_env.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))+"/scripts")
--- build_env/scripts/yaml_intf.py
+++ build_env/scripts/yaml_intf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 import yaml
 import sys
 from operator import attrgetter
