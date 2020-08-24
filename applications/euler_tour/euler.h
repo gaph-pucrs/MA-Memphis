ec306eec935bf66d383f8e3470045f42f275c4bf
Angelo Elias Dalzotto <angelodalzotto97@gmail.com>
2020-08-24 15:46:43 -0300

    Add euler_tour application

---
+++ applications/euler_tour/euler.h
@@ -0,0 +1,76 @@
+/**
+ * Euler tour application for Memphis
+ * 
+ * @file euler.h
+ * 
+ * @author Angelo Elias Dalzotto (angelo.dalzotto@edu.pucrs.br)
+ * GAPH - Hardware Design Support Group (https://corfu.pucrs.br/)
+ * PUCRS - Pontifical Catholic University of Rio Grande do Sul (http://pucrs.br/)
+ * 
+ * @brief This file models the graph and defines the application level message
+ * protocol
+ * 
+ * @date June 2018
+ */
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define NODE_M 0
+#define NODE_A 1
+#define NODE_B 2
+#define NODE_C 3
+
+/* Graph model */
+/* THE GRAPH MUST BE CONNECTED */
+
+#define TOTAL_EDGES 3
+#define TOTAL_TASKS 4
+const int P[TOTAL_TASKS] = {manager, node_a, node_b, node_c};
+
+// Node degree. First always 0
+const int deg[TOTAL_TASKS] = {0, 2, 2, 2};
+
+const char *start_msg[TOTAL_TASKS] = {"Start M",
+									"Start A",
+									"Start B",
+									"Start C"};
+const char *exit_msg[TOTAL_TASKS] = {"Exit M",
+									"Exit A",
+									"Exit B",
+									"Exit C"};
+const char *rcv_msg[TOTAL_TASKS] = {"Rcv M",
+								"Rcv A",
+								"Rcv B",
+								"Rcv C"};
+const char *bkt_msg[TOTAL_TASKS] = {"Bkt M",
+									"Bkt A",
+									"Bkt B",
+									"Bkt C"};
+const char *fwd_msg[TOTAL_TASKS] = {"Fwd M",
+										"Fwd A",
+										"Fwd B",
+										"Fwd C"};
+
+									// m, a, b, c
+const int ntoa[TOTAL_TASKS][TOTAL_TASKS] = {{0, 0, 0, 0}, //m
+									{0, 0, 0, 1},   //a
+									{0, 0, 0, 1},   //b
+									{0, 0, 1, 0}};   //c
+
+const int atop[TOTAL_TASKS][TOTAL_TASKS] = {{0, 0, 0, 0},
+											{2, 3, 0, 0},
+											{1, 3, 0, 0},
+											{1, 2, 0, 0}};
+
+
+// DONT CHANGE
+#define LENGHT 4
+#define MSG_OP msg.msg[0]
+#define MSG_SRC msg.msg[1]
+#define TUNUE msg.msg[2]
+#define BK_ADDR msg.msg[3]
+
+#define FORWARD 1
+#define BACKTRACK -1
+#define EXIT 0
---
+++ applications/euler_tour/node_a.c
@@ -0,0 +1,93 @@
+/**
+ * Euler tour application for Memphis
+ * 
+ * @file node_a.c
+ * 
+ * @author Angelo Elias Dalzotto (angelo.dalzotto@edu.pucrs.br)
+ * GAPH - Hardware Design Support Group (https://corfu.pucrs.br/)
+ * PUCRS - Pontifical Catholic University of Rio Grande do Sul (http://pucrs.br/)
+ * 
+ * @brief This file has the source code for a node and must be replicated for 
+ * each new node added to the application.
+ * 
+ * @date June 2018
+ */
+
+#include <api.h>
+#include <stdbool.h>
+#include "euler.h"
+
+
+/* Node Model */
+#define THIS_NODE NODE_A
+// Number of neighbors
+#define N_ADJ 2
+// Edges weight           to: b  c
+unsigned int weight[N_ADJ] = {0, 0};
+
+
+//DONT CHANGE
+unsigned int counterf = 0;
+unsigned int node_i = 0;
+unsigned int backtracks = 0;
+
+Message msg;
+
+void next_hop(int appid)
+{
+	MSG_SRC = THIS_NODE;
+	if(N_ADJ - counterf - backtracks > 1)
+		BK_ADDR = THIS_NODE;
+
+	if(N_ADJ - counterf - backtracks){ // Send Forward
+		MSG_OP = FORWARD;
+		TUNUE--;
+		for(int i = 0; i < N_ADJ; i++){
+			if(!weight[i]){
+				weight[i] = ++counterf;
+				Echo(fwd_msg[atop[THIS_NODE][i]]);
+				SSend(&msg, (appid << 8) | P[atop[THIS_NODE][i]]);
+				break;
+			}
+		}
+	} else { // Send Backtrack
+		MSG_OP = BACKTRACK;
+		Echo(bkt_msg[BK_ADDR]);
+		SSend(&msg, (appid << 8) | P[BK_ADDR]);
+	}
+}
+
+
+int main()
+{
+	int appid = get_appid();    
+	Echo(start_msg[THIS_NODE]);
+	while(true){
+		SReceive(&msg);
+
+		if(!MSG_OP){        // Exit
+			Echo(exit_msg[THIS_NODE]);
+			exit();
+		} else if(!TUNUE){
+			MSG_OP = EXIT;
+			Echo("No more unused edges. Exiting.");
+			SSend(&msg, (appid << 8) | P[0]);
+		} else if(MSG_OP == FORWARD){
+			Echo(rcv_msg[MSG_SRC]);
+			if(MSG_SRC)
+				weight[ntoa[THIS_NODE][MSG_SRC]] = ++counterf;
+			next_hop(appid);
+		} else { // Backtrack
+			// Find backtracked edge
+			Echo(bkt_msg[MSG_SRC]);
+			int top = 0;
+			for(int i = 0; i < N_ADJ; i++){
+				if(weight[i]>weight[top])
+					top = i;
+			}
+			weight[top] = N_ADJ - backtracks;
+			backtracks++;
+			next_hop(appid);
+		}
+	}
+}
