7ab24726bc877ac55c208494e68422b223c461b7
Leonardo Vian Erthal (Aluno IC - Moraes) <leonardo.erthal@paxos.inf.pucrs.br>
2020-11-25 13:25:15 -0300

    Adicionado uma mensagem de tempo quando o app termina.

--- applications/management/migration_task.c
+++ applications/management/migration_task.c
@@ -15,10 +15,10 @@ int main()
 	/* Action: trigger migration for task 257. Mapper will decide where to migrate the task */
 	Message msg;
 	msg.msg[0] = TASK_MIGRATION_MAP;
-	msg.msg[1] = 257;
+	msg.msg[1] = 516;
 	msg.length = 2;
 
-	while(GetTick() < 100000);
+	while(GetTick() < 800000);
 
 	Echo("Requesting migration for task "); Echo(itoa(msg.msg[1])); Echo("\n");
 
