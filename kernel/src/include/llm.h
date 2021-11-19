1a9ffc3b8ddf565550f65f8f03cd555cba898a72
Angelo Dalzotto <angelodalzotto97@gmail.com>
2021-11-19 10:51:13 -0300

    Update LLM to make fair comparison to BrNoC

--- kernel/src/include/llm.h
+++ kernel/src/include/llm.h
@@ -14,16 +14,6 @@
 #include "task_control.h"
 
 /**
- * @brief Runs the LLM procedure for a task.
- * 
- * @details
- * Grep the task status and sends to the configured observer task.
- * 
- * @param task Pointer to the desired tcb to be monitored.
- */
-void llm_task(tcb_t *task);
-
-/**
  * @brief Run the LLM procedure for RT tasks.
  * 
  * @param tasks TCB array
--- kernel/src/llm.c
+++ kernel/src/llm.c
@@ -16,33 +16,32 @@
 #include "syscall.h"
 #include "services.h"
 
-void llm_task(tcb_t *task)
-{
-	/* What parameters? Deadline? */
-	/* Maybe send a full-featured status */
-	/* Deadline, execution time, etc. LLM should not process too much info */
-
-	/* Build a message */
-	if(task->id != -1 && task->observer_task != -1 && task->scheduler.deadline != -1){
-		int message[5] = {
-			MONITOR, 
-			task->id,
-			task->scheduler.waiting_msg,
-			task->scheduler.slack_time, 
-			task->scheduler.remaining_exec_time
-		};
-		os_kernel_writepipe(task->observer_task, task->observer_address, 5, message);
-	}
-}
-
 void llm_rt(tcb_t *tasks)
 {
-	static unsigned last_rt = 0;
+	static unsigned last_rt[PKG_MAX_LOCAL_TASKS];
+	
 	unsigned now = MMR_TICK_COUNTER;
-	if(now - last_rt > PKG_MONITOR_INTERVAL_QOS){
-		last_rt = now;
-		for(int i = 0; i < PKG_MAX_LOCAL_TASKS; i++){
-			llm_task(&tasks[i]);
+
+	for(int i = 0; i < PKG_MAX_LOCAL_TASKS; i++){
+		int id = tasks[i].id;
+
+		if(id == -1 || (id >> 8) == 0 || tasks[i].observer_task == -1 || tasks[i].scheduler.deadline == -1 || tasks[i].proc_to_migrate != -1)
+			continue; /* Don't send MA task status or non-RT tasks or non-existent tasks or tasks marked to migrate */
+
+		if(now - last_rt[i] >= PKG_MONITOR_INTERVAL_QOS){
+			int message[5] = {
+				MONITOR, 
+				id,
+				tasks[i].scheduler.waiting_msg,
+				tasks[i].scheduler.slack_time, 
+				tasks[i].scheduler.remaining_exec_time
+			};
+
+			if(!MMR_DMNI_SEND_ACTIVE){
+				/* Avoid flooding DMNI and hanging too much time in LLM */
+				os_kernel_writepipe(tasks[i].observer_task, tasks[i].observer_address, 5, message);
+				last_rt[i] = now;
+			}
 		}
 	}
 }
