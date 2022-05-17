b0491f3db802f4adabe3ec5e5276e64726c70fd5
Angelo Elias Dalzotto <angelodalzotto97@gmail.com>
2022-05-17 18:01:47 -0300

    Try to fix router ports

--- hardware/src/RouterCCwrapped2.cpp
+++ hardware/src/RouterCCwrapped2.cpp
@@ -17,39 +17,6 @@
 //input signals
 //read from in, write in internal
 
-void RouterCCwrapped::upd_fail_in(){
-	int i;
-	sc_uint<NPORT > l_fail_in_internal;
-
-	for(i=0;i<NPORT;i++){
-		l_fail_in_internal[i] = fail_in[i].read();
-	}
-
-	fail_in_internal.write(l_fail_in_internal);
-}
-
-void RouterCCwrapped::upd_clock_rx(){
-	int i;
-	sc_uint<NPORT > l_clock_rx_internal;
-
-	 for(i=0;i<NPORT;i++){
-		 l_clock_rx_internal[i] = clock_rx[i].read();
-	 }
-
-	 clock_rx_internal.write(l_clock_rx_internal);
-}
-
-void RouterCCwrapped::upd_eop_in(){
-	int i;
-	sc_uint<NPORT > l_eop_in_internal;
-
-		for(i=0;i<NPORT;i++){
-				//modified to add faults
-				l_eop_in_internal[i] = eop_in[i].read();// & ~fail_in[i].read();
-	 }
-	 eop_in_internal.write(l_eop_in_internal);
-}
-
 void RouterCCwrapped::upd_rx(){
 	int i;
 	sc_uint<NPORT > l_rx_internal;
@@ -77,16 +44,6 @@ void RouterCCwrapped::upd_credit_i(){
 //output signals
 //read from internal, write in output
 
-void RouterCCwrapped::upd_fail_out(){
-	int i;
-	sc_uint<NPORT > l_fail_out_internal;
-
-	l_fail_out_internal = fail_out_internal.read();
-
-	for(i=0;i<NPORT;i++){
-		fail_out[i].write(l_fail_out_internal[i]);
-	}
-}
 
 void RouterCCwrapped::upd_credit_o(){
 	int i;
@@ -97,17 +54,6 @@ void RouterCCwrapped::upd_credit_o(){
 	for(i=0;i<NPORT;i++){
 			//modified to add faults
 			credit_o[i].write(l_credit_o_internal[i] | fail_out[i].read());
-	}
-}
-
-void RouterCCwrapped::upd_clock_tx(){
-	int i;
-	sc_uint<NPORT > l_clock_tx_internal;
-
-	l_clock_tx_internal = clock_tx_internal.read();
-
-	for(i=0;i<NPORT;i++){
-		clock_tx[i].write(l_clock_tx_internal[i]);
 	}
 }
 
@@ -123,37 +69,21 @@ void RouterCCwrapped::upd_tx(){
 	}
 }
 
-void RouterCCwrapped::upd_eop_out(){
-	int i;
-	sc_uint<NPORT > l_eop_out_internal;
-
-	l_eop_out_internal = eop_out_internal.read();
-
-	for(i=0;i<NPORT;i++){
-		//modified to add faults
-		eop_out[i].write(l_eop_out_internal[i]);// & ~fail_out[i].read());
-	}
-}
-
-
 void RouterCCwrapped::traffic_monitor(){
-	int i,aux2;
-	char aux[255]; 
-	unsigned char SM_traffic_monitor_old[NPORT];
+	int i;
+	char aux[255];
 	FILE *fp;
 
-	if(reset.read() == 1){
+	if(reset_n.read() == 0){
 		for(i = 0; i < NPORT; i++) {
 			bandwidth_allocation[i] = 0;
-			SM_traffic_monitor[i] = 11;
-			counter_target[i] = 0;
-			SR_found[i] = 0;
+			SM_traffic_monitor[i] = 0;
 		}
 	}
 	else if (clock.read() == 1){
 
 		for(i = 0; i < NPORT; i++){
-		
+
 			//New flit coming
 			if(rx[i].read() && credit_o[i]){
 
@@ -164,166 +94,52 @@ void RouterCCwrapped::traffic_monitor(){
 				//from header untill the last packet piece
 				bandwidth_allocation[i]++;
 
-				if( (( address == 260 || address == 516) && SM_traffic_monitor[i] != SM_traffic_monitor_old[i]) && i==9){
-					aux2 = SM_traffic_monitor[i]; 
-					//		cout<<"SM_traffic_monitor[" << i << "]: " << aux2 << endl;
-					//		cout<<"    target:" << target_router[i] << endl;
-					//		cout<<"   service: " << service[i] << endl;
-					//		cout<<"    length: " << payload[i] << endl;
-				}
-				
 
 				switch(SM_traffic_monitor[i])
 				{
 
-					case 11: //consumer_id low
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						target_router[i] = data_in[i].read();
+					case 0: //Header
+						SM_traffic_monitor[i] = 1;
+						target_router[i] = data_in[i].read() & 0xFFFF;
 						header_time[i] = (unsigned int)tick_counter.read();
-						if ((target_router[i] >> 12) == 7){
-							SR_found[i] = 1;
-						}
-						else{//XY
-							if (SR_found[i]){
-								SM_traffic_monitor[i] = 12;
-								payload[i] = data_in[i].read();
-							}
-							else{
-								SM_traffic_monitor[i] = 12;
-							}
-						}	
-					break;
+						
+						//printf("%d   --      %d\n",  (unsigned int)data_in[i].read(), target_router[i]); //debug
+						break;
 
-					case 12: //consumer_id low
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						target_router[i] = ((target_router[i] << 16) + data_in[i].read());
-						if (SR_found[i])
-								SM_traffic_monitor[i] = 2;
-						else
-								SM_traffic_monitor[i] = 0;
-					break;
-
-
-					case 0: //Header high
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-					//	target_router[i] = data_in[i].read();
-						header_time[i] = (unsigned int)tick_counter.read();
-						if ((target_router[i] >> 12) == 7){
-							SR_found[i] = 1;
-						}
-						else{//XY
-							if (SR_found[i]){
-								SM_traffic_monitor[i] = 3;
-								payload[i] = data_in[i].read();
-							}
-							else{
-								SM_traffic_monitor[i] = 1;
-							}
-						}		
+					case 1: //Payload
+						payload[i] = data_in[i].read();
+						SM_traffic_monitor[i] = 2;
+						payload_counter[i] = data_in[i].read();
 						break;					
 
-					case 1: //Header low
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						SM_traffic_monitor[i] = 2;
-					//	target_router[i] = ((target_router[i] << 16) + data_in[i].read());
+					case 2: //Service
+						service[i] = data_in[i].read();
+
+						if(service[i] != 0x40 && service[i] != 0x70 && service[i] != 0x23 && service[i] != 0x00 && service[i] != 0x01 && service[i] != 0x31)
+							SM_traffic_monitor[i] = 5;
+						else
+							SM_traffic_monitor[i] = 3;
 						break;
 
-					case 2: //Payload Size
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						payload[i] = data_in[i].read();
-						if ((payload[i] >> 12) == 7)
-								SM_traffic_monitor[i] = 2;
-						else{					
-								SM_traffic_monitor[i] = 3;
-						}
-						break;          
-					case 3: //Payload Size low
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						payload[i] = ((payload[i] << 16) + data_in[i].read())*2;
-						SM_traffic_monitor[i] = 4;
-						payload_counter[i] = payload[i]-2;
-						if (SR_found[i]){
-							payload_counter[i] -=2 ;						
-						}
-						break;          
+					case 3: //If is task_allocation
 
-					case 4: //Service high
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						service[i] = data_in[i].read();
+						task_id[i] = data_in[i].read();
+						if (service[i] == 0x00 || service[i] == 0x01 || service[i] == 0x31)
+							SM_traffic_monitor[i] = 4;
+						else
+							SM_traffic_monitor[i] = 5;
+
+						break;
+
+					case 4:
+						consumer_id[i] = data_in[i].read();
 						SM_traffic_monitor[i] = 5;
 						break;
-						
-					case 5: //Service low
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						service[i] = (service[i] << 16) + data_in[i].read();
 
-						/*switch(data_in[i].read()){
-						case 0x10:
-						case 0x20:
-						case 0x40:
-						case 0x50:
-						case 0x60:
-						case 0x70:
-						case 0x80:
-						case 0x90:
-						case 0x130:
-						case 0x140:
-						case 0x150:
-						case 0x160:
-						case 0x170:
-						case 0x180:
-						case 0x190:
-						case 0x200:
-						break;
-						default:
-							cout << (unsigned int)address << " ############ ERROR - SERVICE UNKNOWED: " << data_in[i].read() << "Inport " << i << " time: " << (unsigned int)tick_counter.read() << endl;
-						}*/
-
-						if(service[i] != 0x40 && service[i] != 0x70 && service[i] != 0x221 && service[i] != 0x10 && service[i] != 0x20 )
-							SM_traffic_monitor[i] = 10;
-						else
-							SM_traffic_monitor[i] = 6;
-						break;	
-
-					case 6: //If is task_allocation high
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						task_id[i] = data_in[i].read();
-						SM_traffic_monitor[i] = 7;
-
-						break;
-						
-					case 7: //If is task_allocation low
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						task_id[i] = (task_id[i] << 16) + data_in[i].read();
-						if (service[i] == 0x10 || service[i] == 0x20)
-							SM_traffic_monitor[i] = 8;
-						else
-							SM_traffic_monitor[i] = 10;
-
-						break;	
-						
-
-					case 8: //consumer_id high
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						consumer_id[i] = data_in[i].read();
-						SM_traffic_monitor[i] = 9;
-						break;
-						
-					case 9: //consumer_id low
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						consumer_id[i] = (consumer_id[i] << 16) + data_in[i].read();
-						SM_traffic_monitor[i] = 10;
-						break;	
-
-					case 10:
-						SM_traffic_monitor_old[i] = SM_traffic_monitor[i];
-						counter_target[i]++;
-						if (counter_target[i] == 2 && SR_found[i] == 1) {
-							target_router[i] = data_in[i].read();
-						}
+					case 5:
 
 						if (payload_counter[i] == 0 ){
-								
+							
 							//Store in aux the C's string way
 							sprintf(aux, "%s/debug/traffic_router.txt", path.c_str());
 
@@ -333,50 +149,35 @@ void RouterCCwrapped::traffic_monitor(){
 							//aux2 = target_router[i];
 							//unsigned int targetRouter = ((aux2 >> 8) << 4) | (aux2 & 0xFF);
 							unsigned int targetRouter = target_router[i];
-																								
+
+
 							// Open a file called "aux" deferred on append mode
 							fp = fopen (aux, "a");
 
-								// if (service[i] != 0x40 && service[i] != 0x70 && service[i] != 0x221 && service[i] != 0x10 && service[i] != 0x20 ){
-								// 	sprintf(aux, "%d\t%d\t%x\t%d\t%d\t%d\t%d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i, targetRouter);
-								// 	// sprintf(aux, "%8d\t%5d\t%3x\t%5d\t%5d\t%5d\t%5d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i, targetRouter);
-								// } else {
-								// 	if (service[i] == 0x10 || service[i] == 0x20 )
-								// 		sprintf(aux, "%d\t%d\t%x\t%d\t%d\t%d\t%d\t%d\t%d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i, targetRouter, task_id[i], consumer_id[i]);
-								// 		// sprintf(aux, "%8d\t%5d\t%3x\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i, targetRouter, task_id[i], consumer_id[i]);
-								// 	else
-								// 		sprintf(aux, "%d\t%d\t%x\t%d\t%d\t%d\t%d\t%d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i, targetRouter, task_id[i]);
-								// 		// sprintf(aux, "%8d\t%5d\t%3x\t%5d\t%5d\t%5d\t%5d\t%5d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i, targetRouter, task_id[i]);
+							if (service[i] != 0x40 && service[i] != 0x70 && service[i] != 0x23 && service[i] != 0x00 && service[i] != 0x01 && service[i] != 0x31){
+								sprintf(aux, "%d\t%d\t%x\t%d\t%d\t%d\t%d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i*2, targetRouter);
+							} else {
+								if (service[i] == 0x00 || service[i] == 0x01 || service[i] == 0x31)
+									sprintf(aux, "%d\t%d\t%x\t%d\t%d\t%d\t%d\t%d\t%d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i*2, targetRouter, task_id[i], consumer_id[i]);
+								else
+									sprintf(aux, "%d\t%d\t%x\t%d\t%d\t%d\t%d\t%d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i*2, targetRouter, task_id[i]);
 
-								// }
-								if (service[i] != 0x40 && service[i] != 0x70 && service[i] != 0x23 && service[i] != 0x00 && service[i] != 0x01 && service[i] != 0x31){
-									sprintf(aux, "%d\t%d\t%x\t%d\t%d\t%d\t%d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i*2, targetRouter);
-								} else {
-									if (service[i] == 0x00 || service[i] == 0x01 || service[i] == 0x31)
-										sprintf(aux, "%d\t%d\t%x\t%d\t%d\t%d\t%d\t%d\t%d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i*2, targetRouter, task_id[i], consumer_id[i]);
-									else
-										sprintf(aux, "%d\t%d\t%x\t%d\t%d\t%d\t%d\t%d\n", header_time[i], newAdress, service[i], payload[i], bandwidth_allocation[i], i*2, targetRouter, task_id[i]);
+							}
 
-								}
+							fprintf(fp,"%s",aux);
+							fclose (fp);
 
-								fprintf(fp,"%s",aux);
-								fclose (fp);
-								target_router[i] = 0;
-								counter_target[i] = 0;
-								SR_found[i] = 0;
-								payload[i] = 0;
-								payload_counter[i] = 0; 
-								bandwidth_allocation[i] = 0;
-								SM_traffic_monitor[i] = 11;
+							bandwidth_allocation[i] = 0;
+							SM_traffic_monitor[i] = 0;
 						}
-						break;
-				}
-			}else if(bandwidth_allocation[i] > 0){
-				bandwidth_allocation[i]++;
-			}
-		}
 
-	}
+					break;
+					}
+				}else if(bandwidth_allocation[i] > 0)
+					bandwidth_allocation[i]++;	
+			}
+
+		}
 }
 
 
--- hardware/src/include/RouterCCwrapped2.h
+++ hardware/src/include/RouterCCwrapped2.h
@@ -9,50 +9,25 @@
 SC_MODULE(RouterCCwrapped) {
 		sc_in<bool> clock;
 		sc_in<bool> reset;
-		sc_in<bool > clock_rx[NPORT];
 		sc_in<bool > rx[NPORT];
-		sc_in<bool > eop_in[NPORT];
 		sc_in<regflit > data_in[NPORT];
 		sc_out<bool > credit_o[NPORT];
-		sc_out<bool > clock_tx[NPORT];
 		sc_out<bool > tx[NPORT];
-		sc_out<bool > eop_out[NPORT];
 		sc_out<regflit > data_out[NPORT];
 		sc_in<bool > credit_i[NPORT];
-		sc_in<bool >  fail_in[NPORT];
-		sc_out<bool >  fail_out[NPORT];
-
-		sc_out<regflit> 			source;
-		sc_out<regflit> 			target;
-		sc_out<bool> 				w_source_target;
-    	sc_out<regNport >           rot_table[NPORT];
-		sc_out<reg4> w_addr;
-
 
 		//signals to bind in wrapped module
-		sc_signal<regNport > clock_rx_internal;
 		sc_signal<regNport > rx_internal;
-		sc_signal<regNport > eop_in_internal;
 		sc_signal<regNport > credit_o_internal;
-		sc_signal<regNport > clock_tx_internal;
 		sc_signal<regNport > tx_internal;
-		sc_signal<regNport > eop_out_internal;
 		sc_signal<regNport > credit_i_internal;
-		sc_signal<regNport > fail_in_internal;
-		sc_signal<regNport > fail_out_internal;
 
 		RouterCC *router;
 
-		void upd_clock_rx();
 		void upd_rx();
-		void upd_eop_in();
 		void upd_credit_o();
-		void upd_clock_tx();
 		void upd_tx();
-		void upd_eop_out();
 		void upd_credit_i();
-		void upd_fail_in();
-		void upd_fail_out();
 
 		//Traffic monitor
 		sc_in<sc_uint<32 > > tick_counter;
@@ -83,106 +58,34 @@ SC_MODULE(RouterCCwrapped) {
 
 			router = new RouterCC("RouterCC", "RouterCC", 1, generic_list);
 
-			#ifdef DUPLICATED_CHANNEL
-				router->reset(reset);
-				router->clock(clock);
-				router->clock_rx(clock_rx_internal);
-				router->rx(rx_internal);
-				router->eop_in(eop_in_internal);
-				router->fail_in(fail_in_internal);
-				router->fail_out(fail_out_internal);
-				//ports interconnection are mixed due to an issue in SystemC - VHDL integration
-				router->data_in[LOCAL1](data_in[EAST0]);
-				router->data_in[LOCAL0](data_in[EAST1]);
-				router->data_in[SOUTH1](data_in[WEST0]);
-				router->data_in[SOUTH0](data_in[WEST1]);
-				router->data_in[NORTH1](data_in[NORTH0]);
-				router->data_in[NORTH0](data_in[NORTH1]);
-				router->data_in[WEST1](data_in[SOUTH0]);
-				router->data_in[WEST0](data_in[SOUTH1]);
-				router->data_in[EAST1](data_in[LOCAL0]);
-				router->data_in[EAST0](data_in[LOCAL1]);
-				router->credit_o(credit_o_internal);
-				router->clock_tx(clock_tx_internal);
-				router->tx(tx_internal);
-				router->eop_out(eop_out_internal);
-				//ports interconnection are mixed due to an issue in SystemC - VHDL integration
-				router->data_out[LOCAL1](data_out[EAST0]);
-				router->data_out[LOCAL0](data_out[EAST1]);
-				router->data_out[SOUTH1](data_out[WEST0]);
-				router->data_out[SOUTH0](data_out[WEST1]);
-				router->data_out[NORTH1](data_out[NORTH0]);
-				router->data_out[NORTH0](data_out[NORTH1]);
-				router->data_out[WEST1](data_out[SOUTH0]);
-				router->data_out[WEST0](data_out[SOUTH1]);
-				router->data_out[EAST1](data_out[LOCAL0]);
-				router->data_out[EAST0](data_out[LOCAL1]);
-
-				router->rot_table[LOCAL1](rot_table[EAST0]);
-				router->rot_table[LOCAL0](rot_table[EAST1]);
-				router->rot_table[SOUTH1](rot_table[WEST0]);
-				router->rot_table[SOUTH0](rot_table[WEST1]);
-				router->rot_table[NORTH1](rot_table[NORTH0]);
-				router->rot_table[NORTH0](rot_table[NORTH1]);
-				router->rot_table[WEST1](rot_table[SOUTH0]);
-				router->rot_table[WEST0](rot_table[SOUTH1]);
-				router->rot_table[EAST1](rot_table[LOCAL0]);
-				router->rot_table[EAST0](rot_table[LOCAL1]);
-
-				router->credit_i(credit_i_internal);
-			#else
-				router->reset(reset);
-				router->clock(clock);
-				router->clock_rx(clock_rx_internal);
-				router->rx(rx_internal);
-				//ports interconnection are mixed due to an issue in SystemC - VHDL integration
-				router->data_in[LOCAL](data_in[EAST]);
-				router->data_in[SOUTH](data_in[WEST]);
-				router->data_in[NORTH](data_in[NORTH]);
-				router->data_in[WEST](data_in[SOUTH]);
-				router->data_in[EAST](data_in[LOCAL]);
-				router->credit_o(credit_o_internal);
-				router->clock_tx(clock_tx_internal);
-				router->tx(tx_internal);
-				//ports interconnection are mixed due to an issue in SystemC - VHDL integration
-				router->data_out[LOCAL](data_out[EAST]);
-				router->data_out[SOUTH](data_out[WEST]);
-				router->data_out[NORTH](data_out[NORTH]);
-				router->data_out[WEST](data_out[SOUTH]);
-				router->data_out[EAST](data_out[LOCAL]);
-				router->credit_i(credit_i_internal);
-			#endif
-
-			
-				router->target(target);
-				router->source(source);
-				router->w_source_target(w_source_target);
-				router->w_addr(w_addr);
-			
+			router->reset(reset);
+			router->clock(clock);
+			router->rx(rx_internal);
+			//ports interconnection are mixed due to an issue in SystemC - VHDL integration
+			router->data_in[LOCAL](data_in[EAST]);
+			router->data_in[SOUTH](data_in[WEST]);
+			router->data_in[NORTH](data_in[NORTH]);
+			router->data_in[WEST](data_in[SOUTH]);
+			router->data_in[EAST](data_in[LOCAL]);
+			router->credit_o(credit_o_internal);
+			router->tx(tx_internal);
+			//ports interconnection are mixed due to an issue in SystemC - VHDL integration
+			router->data_out[LOCAL](data_out[EAST]);
+			router->data_out[SOUTH](data_out[WEST]);
+			router->data_out[NORTH](data_out[NORTH]);
+			router->data_out[WEST](data_out[SOUTH]);
+			router->data_out[EAST](data_out[LOCAL]);
+			router->credit_i(credit_i_internal);			
 
 			// Cleanup the memory allocated for the generic list
 			for (i = 0; i < 1; i++)
 				free((char*)generic_list[i]);
 
-			SC_METHOD(upd_fail_in);
-			for (i = 0; i < NPORT; i++){
-				sensitive << fail_in[i];
-			}
-
-			SC_METHOD(upd_clock_rx);
-			for (i = 0; i < NPORT; i++){
-				sensitive << clock_rx[i];
-			}
 
 			SC_METHOD(upd_rx);
 			for (i = 0; i < NPORT; i++){
 				sensitive << rx[i];
 				sensitive << fail_out[i];
-			}
-
-			SC_METHOD(upd_eop_in);
-			for (i = 0; i < NPORT; i++){
-				sensitive << eop_in[i];
 			}
 
 			SC_METHOD(upd_credit_i);
@@ -193,17 +96,11 @@ SC_MODULE(RouterCCwrapped) {
 
 			//output
 
-			SC_METHOD(upd_fail_out);
-			sensitive << fail_out_internal;
-
 			SC_METHOD(upd_credit_o);
 			sensitive << credit_o_internal;
 			for (i = 0; i < NPORT; i++){
 				sensitive << fail_out[i];
 			}
-
-			SC_METHOD(upd_clock_tx);
-			sensitive << clock_tx_internal;
 
 			SC_METHOD(upd_tx);
 			sensitive << tx_internal;
@@ -215,9 +112,6 @@ SC_MODULE(RouterCCwrapped) {
 			sensitive << clock;
 			sensitive << reset;
 // 
-			SC_METHOD(upd_eop_out);
-			sensitive << eop_out_internal;
-
 		}
 		~RouterCCwrapped()
 		{}
