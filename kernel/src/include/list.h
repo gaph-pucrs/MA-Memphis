96e1ce121943524c7a1a2ab236a11b20cbfbbfb8
Angelo Elias Dalzotto <angelo.dalzotto@edu.pucrs.br>
2022-08-22 09:03:12 -0300

    Add linked-list implementation

--- kernel/src/include/list.h
+++ kernel/src/include/list.h
@@ -14,9 +14,14 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stddef.h>
 
+/* Forward declaration */
 typedef struct _list_entry list_entry_t;
 
+/**
+ * @brief Entry of the list
+ */
 struct _list_entry {
     list_entry_t *prev;
     list_entry_t *next;
@@ -24,9 +29,13 @@ struct _list_entry {
     void *data;
 };
 
+/**
+ * @brief List definitions 
+ */
 typedef struct _list {
     list_entry_t *head;
     list_entry_t *tail;
+    size_t size;
 } list_t;
 
 /**
@@ -40,7 +49,8 @@ void list_init(list_t *list);
  * @brief Finds an item in the list
  * 
  * @param list Pointer to the list structure
- * @param cmpval Pointer to a value to pass to the custom find function
+ * @param cmpval Pointer to a value to pass to the custom find function or
+ * pointer to stored data
  * @param find Pointer to the find function that receives a pointer to the stored 
  * data and another pointer to the value to find (find(entry->data, cmpval)). If
  * NULL, it will compare the pointer of the entry->data to cmpval.
@@ -63,8 +73,9 @@ void *list_get_data(list_entry_t *entry);
  * 
  * @param list Pointer to list structure
  * @param data Pointer to data to store in list
+ * @return list_entry_t* Pointer to the created entry, NULL if no memory
  */
-void list_push_back(list_t *list, void *data);
+list_entry_t *list_push_back(list_t *list, void *data);
 
 /**
  * @brief Removes an element from list
@@ -73,3 +84,61 @@ void list_push_back(list_t *list, void *data);
  * @param entry Pointer to the element to remove
  */
 void list_remove(list_t *list, list_entry_t *entry);
+
+/**
+ * @brief Gets the first element of the list
+ * 
+ * @param list Pointer to the list structure
+ * @return list_entry_t* Pointer to the first list entry
+ */
+list_entry_t *list_front(list_t *list);
+
+/**
+ * @brief Gets the next entry of a list
+ * 
+ * @param entry Pointer to an entry
+ * @return list_entry_t* Pointer to the next element in the list
+ */
+list_entry_t *list_next(list_entry_t *entry);
+
+/**
+ * @brief Removes the first element of the list
+ * 
+ * @param list Pointer to the list
+ */
+void list_pop_front(list_t *list);
+
+/**
+ * @brief Checks if the list is empty
+ * 
+ * @param list Pointer to the list
+ * @return true When empty
+ * @return false When not empty
+ */
+bool list_empty(list_t *list);
+
+/**
+ * @brief Gets the number of elements in the list
+ * 
+ * @param list Pointer to the list
+ * @return size_t Number of elements
+ */
+size_t list_get_size(list_t *list);
+
+/**
+ * @brief Creates a contiguous vector from the list
+ * 
+ * @param list Pointer to the list
+ * @param data_size Size of each element in the list
+ * @return void* Pointer to the created vector, NULL if no memory
+ */
+void *list_vectorize(list_t *list, size_t data_size);
+
+/**
+ * @brief Clears all elements inside a list
+ * 
+ * @details This does not frees the list itself
+ * 
+ * @param list Pointer to the list
+ */
+void list_clear(list_t *list);
--- kernel/src/list.c
+++ kernel/src/list.c
@@ -1,12 +1,26 @@
-#include <stddef.h>
-#include <stdlib.h>
+/**
+ * MA-Memphis
+ * @file list.c
+ *
+ * @author Angelo Elias Dal Zotto (angelo.dalzotto@edu.pucrs.br)
+ * GAPH - Hardware Design Support Group (https://corfu.pucrs.br/)
+ * PUCRS - Pontifical Catholic University of Rio Grande do Sul (http://pucrs.br/)
+ * 
+ * @date August 2022
+ *
+ * @brief Linked list for Memphis
+ */
 
 #include "list.h"
+
+#include <stdlib.h>
+#include <string.h>
 
 void list_init(list_t *list)
 {
     list->head = NULL;
     list->tail = NULL;
+    list->size = 0;
 }
 
 list_entry_t *list_find(list_t *list, void *cmpval, bool (*find_fnc)(void*, void*))
@@ -30,6 +44,30 @@ void *list_get_data(list_entry_t *entry)
     return entry->data;
 }
 
+list_entry_t *list_push_back(list_t *list, void *data)
+{
+    list_entry_t *entry = malloc(sizeof(list_entry_t));
+
+    if(entry == NULL)
+        return NULL;
+
+    entry->data = data;
+    entry->prev = list->tail;
+    entry->next = NULL;
+
+    if(list->tail != NULL)
+        list->tail->next = entry;
+    
+    list->tail = entry;
+
+    if(list->head == NULL)
+        list->head = entry;
+
+    list->size++;
+
+    return entry;
+}
+
 void list_remove(list_t *list, list_entry_t *entry)
 {
     if(entry == list->head)
@@ -47,21 +85,70 @@ void list_remove(list_t *list, list_entry_t *entry)
     /* Remember to free dinamically allocated members of list entry */
 
     free(entry);
-    entry = NULL;
+
+    list->size--;
 }
 
-void list_push_back(list_t *list, void *data)
+list_entry_t *list_front(list_t *list)
 {
-    list_entry_t *entry = malloc(sizeof(list_entry_t));
-    entry->data = data;
-    entry->prev = list->tail;
-    entry->next = NULL;
+    return list->head;
+}
 
-    if(list->tail != NULL)
-        list->tail->next = entry;
+list_entry_t *list_next(list_entry_t *entry)
+{
+    return entry->next;
+}
+
+void list_pop_front(list_t *list)
+{
+    list_entry_t *entry = list->head;
+
+    list->head = entry->next;
+
+    if(entry->next != NULL)
+        entry->next->prev = NULL;
     
-    list->tail = entry;
+    free(entry);
 
-    if(list->head == NULL)
-        list->head = entry;
+    list->size--;
+}
+
+bool list_empty(list_t *list)
+{
+    return (list->head == NULL);
+}
+
+size_t list_get_size(list_t *list)
+{
+    return list->size;
+}
+
+void *list_vectorize(list_t *list, size_t data_size)
+{
+    list_entry_t *entry = list->head;
+
+    void *vector = malloc(list->size*data_size);
+
+    if(vector == NULL)
+        return NULL;
+
+    while(entry != NULL){
+        memcpy(vector, entry->data, data_size);
+        vector += data_size;
+        entry = entry->next;
+    }
+
+    return vector;
+}
+
+void list_clear(list_t *list)
+{
+    list_entry_t *entry = list->head;
+
+    while(entry != NULL){
+        free(entry->data);
+        list_entry_t *oldentry = entry;
+        entry = entry->next;
+        free(oldentry);
+    }
 }
