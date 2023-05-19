2513e9ed2e0c41d2db5291328d8ecd5d63fab125
Angelo Elias Dalzotto <angelo.dalzotto@edu.pucrs.br>
2023-05-19 16:54:35 -0300

    Remove old build folders

--- modules/scenario.py
+++ modules/scenario.py
@@ -59,8 +59,6 @@ class Scenario:
 		makedirs(self.base_dir+"/flit_sniffer", exist_ok=True)
 		makedirs(self.base_dir+"/management", exist_ok=True)
 		makedirs(self.base_dir+"/applications", exist_ok=True)
-		makedirs(self.base_dir+"/libmemphis/src/include", exist_ok=True)
-		makedirs(self.base_dir+"/libmutils/src/include", exist_ok=True)
 
 		copy("{}/hardware/memphis".format(self.testcase_path), "{}/memphis".format(self.base_dir))
 		copyfile("{}/kernel/kernel.txt".format(self.testcase_path), "{}/bootrom.txt".format(self.base_dir))
