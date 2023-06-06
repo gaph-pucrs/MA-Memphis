a8b1cde1f978390c941e3c78ac6e04e034376c56
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2023-06-06 14:28:33 -0300

    Add A/V RT config

--- applications/audio_video_rt/audio_video_def.h
+++ applications/audio_video_rt/audio_video_def.h
@@ -8,11 +8,13 @@
 #ifndef AUDIO_VIDEO_DEF_H_
 #define AUDIO_VIDEO_DEF_H_
 
+#include "config.h"
+
 #define COMPRESSED_SAMPLES	(8*2)
 
 #define FRAMES				20 //Memphis 4X4 sc demora 7s para cada frame
 
-#define AUDIO_VIDEO_PERIOD	135000
+#define AUDIO_VIDEO_PERIOD	RT_PERIOD
 
 #define IVLC_exe_time		 81000
 #define IVLC_deadline		AUDIO_VIDEO_PERIOD
---
+++ applications/audio_video_rt/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#ifndef RT_PERIOD
+	#define RT_PERIOD 135000
+#endif 
